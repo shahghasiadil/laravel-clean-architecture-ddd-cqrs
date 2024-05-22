4a22cd94ff4e5222ae2f753a785f23d8a36957f2
Shahghasi <adil.shahghasi@gmail.com>
2024-05-22 11:47:41 +0430

    feat: CommandBus impl created

---
+++ src/Application/Bus/IlluminateCommandBus.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Application\Bus;
+
+use Application\Bus\Contracts\CommandBusContract;
+use Illuminate\Bus\Dispatcher;
+
+class IlluminateCommandBus implements CommandBusContract
+{
+
+    public function __construct(
+        protected Dispatcher $bus,
+    ) {}
+
+
+    public function dispatch(Command $command): mixed
+    {
+        return $this->bus->dispatch($command);
+    }
+
+    public function register(array $map): void
+    {
+        $this->bus->map($map);
+    }
+}
