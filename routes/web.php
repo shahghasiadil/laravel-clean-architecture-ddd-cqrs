b343d508ef5c930f3f7ae9372ae5c225656c5d66
Shahghasi <adil.shahghasi@gmail.com>
2024-05-23 21:53:39 +0430

    feat: test route created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Presentation\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', UserController::class);
