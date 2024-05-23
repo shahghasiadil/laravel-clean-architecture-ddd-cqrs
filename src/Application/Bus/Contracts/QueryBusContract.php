217f31107d56f729d4c057e376c0f136c81c4178
Shahghasi <adil.shahghasi@gmail.com>
2024-05-23 21:52:04 +0430

    feat: Command Handler and QueryBusContract pint

--- src/Application/Bus/Contracts/QueryBusContract.php
+++ src/Application/Bus/Contracts/QueryBusContract.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Application\Bus;
+namespace Application\Bus\Contracts;
+
+use Application\Bus\Query;
 
 interface QueryBusContract
 {
-
     public function ask(Query $query): mixed;
 
     public function register(array $map): void;
-
 }
