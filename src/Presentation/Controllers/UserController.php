b6f53186c1b5a97a37358aa81ca6c253feb107e5
Shahghasi <adil.shahghasi@gmail.com>
2024-05-23 23:06:40 +0430

    feat: user form request used

--- src/Presentation/Controllers/UserController.php
+++ src/Presentation/Controllers/UserController.php
@@ -7,7 +7,7 @@ use Application\Bus\Contracts\QueryBusContract;
 use Application\Commands\User\CreateUserCommand;
 use Application\DTOs\User\UserDTO;
 use Application\Queries\User\GetUserByEmailQuery;
-use Illuminate\Http\Request;
+use Presentation\Requests\UserStoreFormRequest;
 
 class UserController extends Controller
 {
@@ -17,11 +17,13 @@ class UserController extends Controller
     ) {
     }
 
-    public function __invoke(Request $request)
+    public function __invoke(UserStoreFormRequest $request)
     {
 
+        $userDto = new UserDTO($request->safe()->name, $request->safe()->email, $request->safe()->password);
+
         $email = $this->commandBus->dispatch(
-            new CreateUserCommand(new UserDTO(fake()->name, fake()->email, fake()->password())),
+            new CreateUserCommand($userDto),
         );
 
         $user = $this->queryBus->ask(
