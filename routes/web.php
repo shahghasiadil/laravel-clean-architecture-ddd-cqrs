27a5b0a77f7c9182de5bb28fdc5a97e804da1a8f
Shahghasi <adil.shahghasi@gmail.com>
2024-05-23 23:08:58 +0430

    feat: api installed

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Presentation\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('users', UserController::class);
