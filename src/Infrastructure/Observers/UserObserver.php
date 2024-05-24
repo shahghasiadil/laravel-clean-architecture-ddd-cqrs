93a188f3fa710895d33b6565820172581c926d0c
Shahghasi <adil.shahghasi@gmail.com>
2024-05-24 16:20:51 +0430

    feat: some new changes in UserObserver

--- src/Infrastructure/Observers/UserObserver.php
+++ src/Infrastructure/Observers/UserObserver.php
@@ -1,17 +1,48 @@
 <?php
+
 namespace Infrastructure\Observers;
 
 use Domain\Entities\User;
 
 class UserObserver
 {
-    public function created(User $user)
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
     {
-
+        // ...
     }
 
-    public function updated(User $user)
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
     {
+        // ...
+    }
 
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        // ...
+    }
+
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        // ...
+    }
+
+    /**
+     * Handle the User "forceDeleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        // ...
     }
 }
