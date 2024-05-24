8d64543418ecc2afe1ac059ebf1b0e395e68006a
Shahghasi <adil.shahghasi@gmail.com>
2024-05-24 16:17:30 +0430

    feat: user observer created

---
+++ src/Infrastructure/Observers/UserObserver.php
@@ -0,0 +1,17 @@
+<?php
+namespace Infrastructure\Observers;
+
+use Domain\Entities\User;
+
+class UserObserver
+{
+    public function created(User $user)
+    {
+
+    }
+
+    public function updated(User $user)
+    {
+
+    }
+}
