b0c0e220013e1d40bb81c66bf88d9794f8a5ff36
Shahghasi <adil.shahghasi@gmail.com>
2024-05-28 21:18:37 +0430

    feat: Domain Provider created

---
+++ src/Domain/Providers/DomainServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Domain\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class DomainServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
