a5484e0e32c5c78d6d1eecaeb4e05bb644252eae
Shahghasi <adil.shahghasi@gmail.com>
2024-05-28 21:18:08 +0430

    feat: Application Service Provider created

---
+++ src/Application/Providers/ApplicationServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Application\Providers;
+
+use Application\Bus\Contracts\CommandBusContract;
+use Application\Bus\Contracts\QueryBusContract;
+use Application\Bus\IlluminateCommandBus;
+use Application\Bus\IlluminateQueryBus;
+use Application\User\CommandHandlers\CreateUserCommandHandler;
+use Application\User\Commands\CreateUserCommand;
+use Application\User\Contracts\UserServiceContract;
+use Application\User\Queries\GetUserByEmailQuery;
+use Application\User\Queries\GetUserByEmailQueryHandler;
+use Application\User\Services\UserService;
+use Illuminate\Support\ServiceProvider;
+
+class ApplicationServiceProvider extends ServiceProvider
+{
+    public $singletons = [
+        CommandBusContract::class => IlluminateCommandBus::class,
+        QueryBusContract::class => IlluminateQueryBus::class,
+        UserServiceContract::class => UserService::class,
+    ];
+
+    public function register(): void
+    {
+        $this->registerCommandHandlers();
+        $this->registerQueryHandlers();
+    }
+
+    public function boot(): void
+    {
+        // Application-specific bootstrapping
+    }
+
+    protected function registerCommandHandlers(): void
+    {
+        $commandBus = app(CommandBusContract::class);
+        $commandBus->register([
+            CreateUserCommand::class => CreateUserCommandHandler::class,
+        ]);
+    }
+
+    protected function registerQueryHandlers(): void
+    {
+        $queryBus = app(QueryBusContract::class);
+        $queryBus->register([
+            GetUserByEmailQuery::class => GetUserByEmailQueryHandler::class,
+        ]);
+    }
+}
