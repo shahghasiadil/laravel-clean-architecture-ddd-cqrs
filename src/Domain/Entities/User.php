7f78bc7deb69ba4edcbf97c3aa6a9a8a0d193cf6
Shahghasi <adil.shahghasi@gmail.com>
2024-05-28 15:56:13 +0430

    fix: Observer namespace changed

--- src/Domain/Entities/User.php
+++ src/Domain/Entities/User.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Infrastructure\Observers\UserObserver;
+use Domain\Observers\UserObserver;
 
 #[ObservedBy([UserObserver::class])]
 class User extends Authenticatable
