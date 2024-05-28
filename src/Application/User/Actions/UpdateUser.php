32b472ce9822d634e6d98e73d4ec5211857cccee
Shahghasi <adil.shahghasi@gmail.com>
2024-05-28 19:51:45 +0430

    feat: UpdateUserAction Created

---
+++ src/Application/User/Actions/UpdateUser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Application\User\Actions;
+
+use Domain\Repositories\UserRepositoryContract;
+use Illuminate\Support\Facades\DB;
+
+class UpdateUser
+{
+    public function __construct(private UserRepositoryContract $userRepository)
+    {
+    }
+
+    public function __invoke(int $id, string $name, string $email)
+    {
+        return DB::transaction(function () use ($id, $name, $email) {
+           return $this->userRepository->update($id, $name, $email);
+        });
+
+    }
+}
