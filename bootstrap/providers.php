23db0660ad75bcfc9303d64e327adf460e90e6b4
Shahghasi <adil.shahghasi@gmail.com>
2024-05-28 21:19:21 +0430

    feat: providers namespaces fixed

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    Application\Providers\ApplicationServiceProvider::class,
+    Domain\Providers\DomainServiceProvider::class,
+    Infrastructure\Providers\InfrastructureServiceProvider::class,
 ];
