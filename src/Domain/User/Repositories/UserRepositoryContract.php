bb2b697f1450d9e101f6481fd7639b361b39ab2a
Shahghasi <adil.shahghasi@gmail.com>
2024-06-02 18:25:52 +0430

    feat: UserData replaced with Dto's

--- src/Domain/User/Repositories/UserRepositoryContract.php
+++ src/Domain/User/Repositories/UserRepositoryContract.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Domain\User\Repositories;
 
-use Application\User\DTOs\CreateUserDTO;
+use Application\User\Data\UserData;
 use Domain\User\Entities\User;
 use Illuminate\Database\Eloquent\Collection;
 
 interface UserRepositoryContract
 {
-    public function save(CreateUserDTO $dto): string;
+    public function save(UserData $data): string;
 
     public function findByEmail(string $email): ?User;
 
