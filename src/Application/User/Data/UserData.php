1b0b3210c1c555ed9ef6b35491625595d712e4cb
Shahghasi <adil.shahghasi@gmail.com>
2024-06-02 18:18:45 +0430

    feat: user data created

--- src/Application/User/Data/UserData.php
+++ src/Application/User/Data/UserData.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Application\User\Data;
 
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Optional;
 
 class UserData extends Data
 {
-    public function __construct(public int $id, public string $name, public string $email, public string $password)
-    {
-        $this->id = $id;
-        $this->name = $name;
-        $this->email = $email;
-        $this->password = $password;
-    }
+    public function __construct(
+        public int|Optional $id,
+        public string $name,
+        public string $email,
+        public string $password,
+    ) {}
 }
