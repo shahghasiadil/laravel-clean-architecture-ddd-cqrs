875326260345a374352b196c1f0acc8910a88456
Shahghasi <adil.shahghasi@gmail.com>
2024-06-04 21:17:39 +0430

    Ref: Bool return instead of int in update of user in UserRepoContract

--- src/Domain/User/Repositories/UserRepositoryContract.php
+++ src/Domain/User/Repositories/UserRepositoryContract.php
@@ -16,5 +16,5 @@ interface UserRepositoryContract
 
     public function getAllUsers(): Collection;
 
-    public function update(int $id, UserData $data): int;
+    public function update(int $id, UserData $data): bool;
 }
