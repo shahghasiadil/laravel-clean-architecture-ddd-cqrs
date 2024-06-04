025108745183dd0502dc37d5d000519b4f892c2a
Shahghasi <adil.shahghasi@gmail.com>
2024-06-04 21:37:03 +0430

    Ref: code reforamted

--- src/Shared/Traits/EnumValues.php
+++ src/Shared/Traits/EnumValues.php
@@ -1,11 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Shared\Enums\Traits;
 
-trait EnumValues {
+trait EnumValues
+{
     public static function values(): array
     {
-       return array_column(self::cases(), 'value');
+        return array_column(self::cases(), 'value');
     }
 
     public static function forSelect(): array
