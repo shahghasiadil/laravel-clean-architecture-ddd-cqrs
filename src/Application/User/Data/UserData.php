a49491190da44ce3887fff2b809369f776ee70ea
Shahghasi <adil.shahghasi@gmail.com>
2024-06-04 21:40:30 +0430

    feat: user status added to user data

--- src/Application/User/Data/UserData.php
+++ src/Application/User/Data/UserData.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Application\User\Data;
 
+use Shared\Enums\UserStatus;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Optional;
 
@@ -12,6 +13,7 @@ class UserData extends Data
     public function __construct(
         public string $name,
         public string $email,
+        public UserStatus $status,
         public string|Optional $password,
     ) {}
 }
--- src/Domain/User/Entities/User.php
+++ src/Domain/User/Entities/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            'status' => UserStatus::class
+            'status' => UserStatus::class,
         ];
     }
 }
