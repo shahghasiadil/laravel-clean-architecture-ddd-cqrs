24fa798972f7b7f6a7a20a4fa6715e75b1480647
Shahghasi <adil.shahghasi@gmail.com>
2024-06-04 21:18:38 +0430

    feat: password as optional in userService update

--- src/Application/User/Data/UserData.php
+++ src/Application/User/Data/UserData.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace Application\User\Data;
 
 use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Optional;
 
 class UserData extends Data
 {
     public function __construct(
         public string $name,
         public string $email,
-        public string $password,
+        public string|Optional $password,
     ) {}
 }
