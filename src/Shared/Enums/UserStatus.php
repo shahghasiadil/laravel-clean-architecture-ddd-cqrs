213041e33b21354bb0169ad159b6372e4608b24a
Shahghasi <adil.shahghasi@gmail.com>
2024-06-04 21:35:21 +0430

    feat: trait added to enum

--- src/Shared/Enums/UserStatus.php
+++ src/Shared/Enums/UserStatus.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Shared\Enums;
 
+use Shared\Enums\Traits\EnumValues;
+
 enum UserStatus: string
 {
+    use EnumValues;
     case Active = 'active';
     case Inactive = 'inactive';
     case Suspended = 'suspended';
