c7884e6d8c16d25af3d7a8509b958f3e46cc1379
Shahghasi <adil.shahghasi@gmail.com>
2024-06-04 21:36:24 +0430

    feat: enum for select helper created

--- src/Shared/Traits/EnumValues.php
+++ src/Shared/Traits/EnumValues.php
@@ -7,4 +7,9 @@ trait EnumValues {
     {
        return array_column(self::cases(), 'value');
     }
+
+    public static function forSelect(): array
+    {
+        return array_column(self::cases(), 'name', 'value');
+    }
 }
