6b8948b4b4e6199ff0106533c78f8597304995f5
Shahghasi <adil.shahghasi@gmail.com>
2024-06-04 22:19:16 +0430

    feat: UserStatus added to userlist data

--- src/Application/User/Data/UsersListData.php
+++ src/Application/User/Data/UsersListData.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Application\User\Data;
 
 use Carbon\Carbon;
+use Shared\Enums\UserStatus;
 use Spatie\LaravelData\Data;
 
 class UsersListData extends Data
@@ -12,6 +13,7 @@ class UsersListData extends Data
     public int $id;
     public string $name;
     public string $email;
+    public UserStatus $status;
     public Carbon $created_at;
     public Carbon $updated_at;
 }
