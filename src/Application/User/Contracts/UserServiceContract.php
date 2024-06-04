74db1d60cfa2f1880601c8476258d81ae8fed526
Shahghasi <adil.shahghasi@gmail.com>
2024-06-04 21:13:13 +0430

    feat: User Service Contract created

--- src/Application/User/Contracts/UserServiceContract.php
+++ src/Application/User/Contracts/UserServiceContract.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Application\User\Contracts;
 
+use Application\User\Data\UserData;
 use Illuminate\Database\Eloquent\Collection;
 
 interface UserServiceContract
 {
     public function index(): Collection;
+
+ public function update($id, UserData $userData) : int;
 }
