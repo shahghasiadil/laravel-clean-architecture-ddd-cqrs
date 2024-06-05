0f85ffaace78fe2cdc8d64a56181b1c14e0b7e86
Shahghasi <adil.shahghasi@gmail.com>
2024-06-05 21:46:13 +0430

    feat: api added

---
+++ src/Presentation/UserManagement/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Presentation\UserManagement\Controllers\UserController;
+
+Route::get('/user', fn(Request $request) => $request->user())->middleware('auth:sanctum');
+
+Route::controller(UserController::class)->group(function (): void {
+    Route::post('users/create', 'store');
+    Route::put('users/{id}/update', 'update');
+});
