c136d6170744ab4c0089ee303f88e4a294de25ba
Shahghasi <adil.shahghasi@gmail.com>
2024-06-11 21:53:39 +0430

    feat: route created for get users

--- src/Presentation/UserManagement/routes/api.php
+++ src/Presentation/UserManagement/routes/api.php
@@ -11,4 +11,5 @@ Route::get('/user', fn(Request $request) => $request->user())->middleware('auth:
 Route::controller(UserController::class)->group(function (): void {
     Route::post('users/create', 'store');
     Route::put('users/{id}/update', 'update');
+    Route::get('users', 'index');
 });
