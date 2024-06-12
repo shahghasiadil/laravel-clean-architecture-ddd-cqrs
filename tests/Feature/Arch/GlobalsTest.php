b4c569bea17ac6210efcd20fae2c91a45184ef0a
Shahghasi <adil.shahghasi@gmail.com>
2024-06-12 15:52:24 +0430

    feat: strict typing test added

--- tests/Feature/Arch/GlobalsTest.php
+++ tests/Feature/Arch/GlobalsTest.php
@@ -5,3 +5,7 @@ declare(strict_types=1);
 arch('globals')
     ->expect(['dd', 'dump', 'ray'])
     ->not->toBeUsed();
+
+arch('strict typing in src dir')
+    ->expect('src')
+    ->toUseStrictTypes();
