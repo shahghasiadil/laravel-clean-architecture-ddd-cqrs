cc5fc2f5b05c484e03f67307f0ca2b87b7651c65
Shahghasi <adil.shahghasi@gmail.com>
2024-06-12 21:58:40 +0430

    feat: infras test

--- tests/Architecture/GlobalsTest.php
+++ tests/Architecture/GlobalsTest.php
@@ -14,5 +14,12 @@ arch('Domain can be accessed by Infrastructure and Application Layers')
     ->expect('Domain')
     ->toOnlyBeUsedIn(['Infrastructure', 'Application']);
 
+arch('Infrastructure should only access Domain and Shared Resources layers')
+    ->expect('Infrastructure')
+    ->toOnlyBeUsedIn(['Domain', 'Shared']);
+
+arch('Infrastructure should not access the Presentation Layer directly')
+    ->expect('Infrastructure')
+    ->not->toBeUsedIn(['Presentation']);
 
 
