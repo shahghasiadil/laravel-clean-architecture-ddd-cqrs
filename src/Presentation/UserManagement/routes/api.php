7172ace07688c9781601b6da631f094f29db9059
Shahghasi <adil.shahghasi@gmail.com>
2025-02-07 10:49:46 +0430

    feat: routes changed

--- src/Presentation/UserManagement/routes/api.php
+++ src/Presentation/UserManagement/routes/api.php
@@ -9,7 +9,7 @@ use Presentation\UserManagement\Controllers\UserController;
 Route::get('/user', fn(Request $request) => $request->user())->middleware('auth:sanctum');
 
 Route::controller(UserController::class)->group(function (): void {
-    Route::post('users/create', 'store');
-    Route::put('users/{id}/update', 'update');
+    Route::post('users', 'store');
+    Route::patch('users/{id}', 'update');
     Route::get('users', 'index');
 });
