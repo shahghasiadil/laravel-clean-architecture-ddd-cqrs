265d8cdd99f980697882661201d43ff6e869e07e
Shahghasi <adil.shahghasi@gmail.com>
2025-02-07 11:17:04 +0430

    feat: remove api platform

--- src/Domain/User/Entities/User.php
+++ src/Domain/User/Entities/User.php
@@ -11,10 +11,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Shared\Enums\UserStatus;
-use ApiPlatform\Metadata\ApiResource;
 
 #[ObservedBy([UserObserver::class])]
-#[ApiResource]
 class User extends Authenticatable
 {
     use HasFactory;
