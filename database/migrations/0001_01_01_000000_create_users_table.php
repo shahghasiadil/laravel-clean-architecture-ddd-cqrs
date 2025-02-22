0394534bec3fb800c262ecb15a74f939f2992d4b
Shahghasi <adil.shahghasi@gmail.com>
2025-02-22 10:44:20 +0430

    ref: enum location changed

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
+use Domain\User\Enums\UserStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Shared\Enums\UserStatus;
 
 return new class extends Migration {
     /**
